03ac828799a5622840e3ab68e183b24982b7761a
Paras-is-coding <paraschand5815@gmail.com>
2023-10-08 17:00:55 +0545

    connected mongoose database

--- src/app/config/express.config.js
+++ src/app/config/express.config.js
@@ -1,4 +1,15 @@
 const express = require("express");
+
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+dotenv.config();
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
+    console.log("MongoDB connected succcessfully!");
+}).catch((err)=>{
+    console.log(err);
+})
+
+
 const router = require('../router/index');
 const app = express();
 
