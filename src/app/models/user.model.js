c6d6bf05e2772016a7319e2c156cd0ebd9aff130
Paras-is-coding <paraschand5815@gmail.com>
2023-10-08 17:32:00 +0545

    created user model

---
+++ src/app/models/user.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+//create rows or schema
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+    }    
+},
+{timestamps:true});
+
+//create model 
+const User = mongoose.model('User',userSchema);
+
+export default User;
